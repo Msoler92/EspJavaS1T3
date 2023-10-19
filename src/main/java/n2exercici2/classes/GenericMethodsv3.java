df2db694def5618ce96e054a0d3bcc0f72ca4008
Manel Sole <msoler92@gmail.com>
2023-10-19 10:21:20 +0200

    N1, N2 and N3 completed

--- src/main/java/n2exercici2/classes/GenericMethodsv3.java
+++ src/main/java/n2exercici2/classes/GenericMethodsv3.java
@@ -1,6 +1,7 @@
 package n2exercici2.classes;
 
 public class GenericMethodsv3 {
+    @SafeVarargs
     public static <T> void genericPrint(T... varargs) {
         for (T t: varargs) {
             System.out.println(t);
